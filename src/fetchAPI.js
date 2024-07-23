604aae593c1274c8ffbb08f2ff5c092b61ea4d35
kpbongat <kpbongat.dev@gmail.com>
2024-07-23 16:50:22 +0800

    Change unit group to metric

--- src/fetchAPI.js
+++ src/fetchAPI.js
@@ -2,7 +2,7 @@ export default function fetchAPI(location) {
   const API_URL =
     "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline";
   const FREE_API_KEY = "4HQQSPAE9XGZAA2G9LXJ7QNFZ";
-  const requestURL = `${API_URL}/${location}?unitGroup=us&elements=datetime%2Ctemp%2Cprecip%2Cprecipprob%2Cuvindex&include=hours&key=${FREE_API_KEY}`;
+  const requestURL = `${API_URL}/${location}?unitGroup=metric&elements=datetime%2Ctemp%2Cprecip%2Cprecipprob%2Cuvindex&include=hours&key=${FREE_API_KEY}`;
   const request = new Request(requestURL);
   const response = fetch(request);
   return response;
